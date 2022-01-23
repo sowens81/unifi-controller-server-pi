b55efef552e6088d73513c72827827626adfdbd2
Steve Owens <stevejowens81@outlook.com>
2022-01-23 10:25:42 +0000

    feature/AB#00001: added deployment script

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Ubiquiti Unfi Cloud Controller - Raspberry Pi Deployment
+
+x.
+
+## Installation
+
+Use the scripting tool.
+
+```bash
+git
+```
+
+## Usage
+
+```bash
+unifi-ubuntu-setup.sh
+```
+
+## Contributing
+x
+
+## License
+x
---
+++ unifi-ubuntu-setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#Unifi Controller Server Setup
+
+hostName="unifi-cloud"
+networkInterface="eth0"
+ipAddress="192.168.0.2"
+networkMask="24"
+gatewayIP="192.168.0.1"
+dnsServers='192.168.0.1, 8.8.8.8' 
+unifiControlSoftware = "https://fw-download.ubnt.com/data/unifi-controller/d41a-debian-6.5.55-1906672766bc49aa990c952117af3dd3.deb"
+
+# Configure Server Static IP Address
+sed -i "s/netowrkInterface/$networkInterface/" config/01-netcfg.yaml
+sed -i "s/ipAddress/$ipAddress/" config/01-netcfg.yaml
+sed -i "s/networkMask/$networkMask/" config/01-netcfg.yaml
+sed -i "s/gatewayIPAddress/$gatewayIP/" config/01-netcfg.yaml
+sed -i "s/dnsIPAddresses/$dnsServers/" config/01-netcfg.yaml
+
+cp configf/99-disable-network-config.cfg  /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+
+cp config/01-netcfg.yaml  /etc/netplan/config/01-netcfg.yaml
+
+netplan apply
+
+# Update Server Hostname
+currentHostName=(hostnamectl) 
+sed -i "s/$currentHostName/$hostName/" "/etc/hosts"
+
+# Update and Upgrade Packages
+apt-get update
+apt-get upgrade
+systemctl enable ssh
+service ssh start
+
+# Enable SSH Access
+apt-get install openssh-server
+
+# Unifi Controller Install
+wget -O unifi_sysvinit_all.deb $unifiControlSoftware
+apt-get install -f
+systemctl enable unifi
+service unifi start
+allow ssh
+
+# Restart
+reboot
+
+
+
+
+
